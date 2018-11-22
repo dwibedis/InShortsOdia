3920ebf9656495d0354624a9d90a8df432991c31
SatyaPrakash <43237478+dwibedis@users.noreply.github.com>
2018-11-22 15:13:22 +0530

    Added changes to allow cross origin request

--- src/main/java/com/inshorts/odia/controller/HomeController.java
+++ src/main/java/com/inshorts/odia/controller/HomeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.inshorts.odia.service.NewsService;
 
+@CrossOrigin
 @RestController
 public class HomeController {
 
